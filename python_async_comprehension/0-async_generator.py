154aa259d680f5ccee3f88146cce890bfd7b9690
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-05-10 21:59:52 +0200

    Python - Async Comprehension

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""The coroutine takes no argumanets and will loop 10 times,
+"""The coroutine takes no argumanets and will loop 10 times
 each time asynchronously wait 1 second, then yield a random
 number between 0 and 10. Use the random module.
 """
