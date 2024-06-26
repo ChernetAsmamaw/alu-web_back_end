8feb1498561adcfc579667a75697d55e2eea2952
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-06-26 14:58:56 +0300

    simple flask app

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -5,8 +5,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+# index function to render the index.html template
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run()
