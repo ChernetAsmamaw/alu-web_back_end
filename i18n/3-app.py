cdc4d4e6df08d8f2f09eb6560ab7f76c83a4c8c8
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-06-27 16:32:34 +0300

    updates

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -2,7 +2,7 @@
 """ Setup a basic Flask app  """
 
 from flask import Flask, request, render_template
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -22,7 +22,7 @@ app.config.from_object(Config)
 def get_locale():
     """ determine the best match with our supported languages """
     return request.accept_languages.best_match(app.config.LANGUAGES)
-                                               
+                                         
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
