95739b5e2894e97b85dce7f9fac6c2dd088e0062
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-06-27 16:16:58 +0300

    internationalization flask-babel

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' Flask app '''
+""" Setup a basic Flask app  """
 
 from flask import Flask, request, render_template
 from flask_babel import Babel
@@ -9,7 +9,7 @@ babel = Babel(app)
 
 
 class Config:
-    ''' App config '''
+    """ Config class for Babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,13 +20,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    ''' return best languages '''
+    """ determine the best match with our supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def hello_world():
-    ''' return the template '''
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ index function to render a simple html template """
     return render_template('2-index.html')
 
 
--- i18n/6-app.py
+++ i18n/6-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Setup a basic Flask app  """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, g
 from flask_babel import Babel, gettext
 
 app = Flask(__name__)
@@ -17,12 +17,25 @@ class Config:
 
 app.config.from_object(Config)
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@app.before_request
+def before_request():
+    """ call get_user function before each request """
+    g.user = get_user()
+
 
 @babel.localeselector
 def get_locale():
     """ determine the best match with our supported languages """
     locale = request.args.get('locale')
-    if locale:
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config.LANGUAGES)
                                                
@@ -30,7 +43,15 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ index function to render a simple html template """
-    return render_template('4-index.html')
+    return render_template('6-index.html')
+
+
+def get_user():
+    """ get user from request """
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users.get(int(user_id))
+    return None
 
 
 if __name__ == '__main__':
