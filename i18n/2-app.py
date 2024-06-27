d1ea795712e051bc3a972adeccf3209ea355920d
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-06-27 13:59:20 +0300

    update

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Setup a basic Flask app  """
+''' Flask app '''
 
 from flask import Flask, request, render_template
 from flask_babel import Babel
@@ -9,7 +9,7 @@ babel = Babel(app)
 
 
 class Config:
-    """ Config class for Babel """
+    ''' App config '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,13 +20,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """ determine the best match with our supported languages """
+    ''' return best languages '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """ index function to render a simple html template """
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world():
+    ''' return the template '''
     return render_template('2-index.html')
 
 
