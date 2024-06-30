97190c00497296b4adecf022a660aae090129786
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-06-30 13:29:24 +0300

    Implementing an expiring web cache and tracker

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,36 +1,56 @@
 #!/usr/bin/env python3
-"""
-Cache web module
-"""
+'''
+Web caching module using Redis
+
+This module provides functions for caching web pages using Redis.
+It includes a decorator `count_requests` to count requests and cache
+responses for a specified duration.
+'''
+
 
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
-rd = redis.Redis()
+# Initialize Redis connection
+redis_connection = redis.Redis()
 
+def count_requests(func: Callable) -> Callable:
+    '''
+    Decorator function to count requests and cache responses in Redis.
 
-def count_requests(method: Callable) -> Callable:
-    """requests"""
+    - func (Callable): expects a URL string as argument.
+    - Callable: caches responses and increments request count.
+    '''
 
-    @wraps(method)
+    @wraps(func)
     def wrapper(url):
-        """wrapper"""
-        rd.incr(f"count:{url}")
-        cached_html = rd.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
+        '''
+        Wrapper function to count requests and cache responses.
 
-        html = method(url)
-        rd.setex(f"cached:{url}", 10, html)
-        return html
+        - url (str): URL of the web page to fetch and cache.
+        - str: Cached HTML content of the web page.
+        '''
+        redis_connection.incr(f"request_count:{url}")  # Increment request count for the URL
+        cached_html = redis_connection.get(f"cached_content:{url}")  # Check if URL is cached
+        if cached_html:
+            return cached_html.decode('utf-8')  # Return cached HTML
+
+        html_content = func(url)  # Fetch HTML content from the URL
+        redis_connection.setex(f"cached_content:{url}", 10, html_content)  # Cache HTML content for 10 seconds
+        return html_content
 
     return wrapper
 
-
 @count_requests
 def get_page(url: str) -> str:
-    """pages"""
-    req = requests.get(url)
-    return req.text
+    '''
+    Function to fetch a web page using HTTP GET request.
+
+    - url (str): URL of the web page to fetch.
+    - str: HTML content of the fetched web page.
+    '''
+    response = requests.get(url)  # Perform HTTP GET request
+    return response.text  # Return HTML content of the response
+
