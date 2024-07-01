5cb647b0f247e6ddf19c9f37eeb5afd9e7a51536
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-07-01 11:43:12 +0300

    selecting bands

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
 -- List all the bands with Glam Rock as their main style, ranked by their longevity
 
-SELECT band_name, COALESCE(split, 2000) - formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam Rock%';
+SELECT band_name, COALESCE(split, 2000) - formed AS lifespan FROM metal_bands
+WHERE style LIKE '%Glam Rock%';
