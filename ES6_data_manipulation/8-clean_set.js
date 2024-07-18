78fe3c7edde6407560711df905feeade9eb75d2e
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-07-18 17:25:39 +0300

    js data manipultion  - updtes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
-// Create a function named cleanSet that returns a string of all the set values that start with a specific string (startString).
+// cleanSet returns a string of all the set values that start with a specific string.
 
 export default function cleanSet(set, startString) {
   if (typeof set !== 'object') return '';
@@ -7,8 +7,7 @@ export default function cleanSet(set, startString) {
 
   const stringSet = [];
   [...set].forEach((item) => {
-    if (item && item.indexOf(startString) === 0)
-        stringSet.push(item.replace(startString, ''));
-    });
-    return stringSet.join('-');
+    if (item && item.indexOf(startString) === 0)stringSet.push(item.replace(startString, ''));
+  });
+  return stringSet.join('-');
 }
