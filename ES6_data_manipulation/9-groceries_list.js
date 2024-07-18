cac51e721b81f99cdb9ef71f65ccb9ac1f188745
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-07-18 15:18:13 +0300

    js data manipultion  - updtes

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,8 @@
-// Create a function named groceriesList that returns a map of groceries with the following items (name, quantity):
+// groceriesList returns a map of groceries with the following items (name, quantity)
 
-export default function groceriesList() {
-  return groceries = new Map()
+export const groceriesList = () => new Map()
   .set('Apples', 10)
   .set('Tomatoes', 10)
   .set('Pasta', 1)
   .set('Rice', 1)
   .set('Banana', 5);
-}
